1b496437f95b00d88c9bfbaf33c62dbd110069a0
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2025-01-21 14:51:46 +0100

    update feature coord gps

--- meteo-app/app/index.tsx
+++ meteo-app/app/index.tsx
@@ -1,12 +1,22 @@
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { s } from './index.style';
 import {Home} from "@/pages/Home/Home";
+import {ImageBackground} from "react-native";
+// @ts-ignore
+import backgroundImg from "@/assets/images/background.png";
 
 export default function Index() {
   return (
-      <SafeAreaProvider>
-          <SafeAreaView>
-              <Home />
-          </SafeAreaView>
-      </SafeAreaProvider>
+      <ImageBackground
+          source={backgroundImg}
+          style={s.img_background}
+          imageStyle={s.img} // style sur l'image directement
+      >
+          <SafeAreaProvider style={s.container}>
+              <SafeAreaView>
+                  <Home />
+              </SafeAreaView>
+          </SafeAreaProvider>
+      </ ImageBackground>
   );
 }
--- meteo-app/pages/Home/Home.jsx
+++ meteo-app/pages/Home/Home.jsx
@@ -1,8 +1,35 @@
 import { s } from './Home.style';
-import {Text} from "react-native";
+import {View} from "react-native";
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
+import {useEffect, useState} from "react";
 
 export function Home() {
+
+    const [coords, setCoords] = useState();
+
+    useEffect(() => {
+        getUserCoords();
+    }, []);
+
+    /**
+     * Permet de demander à l'utilisateur les coordonnées gps
+     * @returns {Promise<void>}
+     */
+    async function getUserCoords() {
+       let { status } =  await requestForegroundPermissionsAsync();
+       if (status === 'granted') {
+           const location = await getCurrentPositionAsync();
+           setCoords({lat: location.coords.latitude, lng:  location.coords.longitude});
+       } else {
+           // Coordonnées de Paris par défault
+           setCoords({lat: "48.85", lng: "2.35"})
+       }
+    }
+
+
     return <>
-        <Text>Hello</Text>
+        <View style={s.meteo_basic}></View>
+        <View style={s.searchbar}></View>
+        <View style={s.meteo_advanced}></View>
     </>
 }
