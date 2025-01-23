6813f4243a54824642c45039d1df29301011424f
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2025-01-23 15:43:24 +0100

    feature navigation

--- meteo-app/app/index.tsx
+++ meteo-app/app/index.tsx
@@ -7,6 +7,12 @@ import backgroundImg from "@/assets/images/background.png";
 // @ts-ignore
 import AlataRegular from '@/assets/fonts/Alata-Regular.ttf'
 import { useFonts } from 'expo-font';
+import {NavigationContainer, NavigationIndependentTree} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {Forecast} from "@/pages/Forecast/Forecast";
+
+// Creation de la pile de routes
+const Stack = createNativeStackNavigator();
 
 export default function Index() {
 
@@ -16,16 +22,34 @@ export default function Index() {
     });
 
   return (
-      <ImageBackground
-          source={backgroundImg}
-          style={s.img_background}
-          imageStyle={s.img} // style sur l'image directement
-      >
-          <SafeAreaProvider>
-              <SafeAreaView style={s.container}>
-                  {isFontLoaded ? <Home /> : null}
-              </SafeAreaView>
-          </SafeAreaProvider>
-      </ ImageBackground>
+      <NavigationIndependentTree>
+        <NavigationContainer >
+              <ImageBackground
+                  source={backgroundImg}
+                  style={s.img_background}
+                  imageStyle={s.img} // style sur l'image directement
+              >
+                  <SafeAreaProvider>
+                      <SafeAreaView style={s.container}>
+                          {isFontLoaded ? (
+                              <Stack.Navigator
+                                  initialRouteName="Home"
+                                  screenOptions={{
+                                      headerShown: false, // Supprime l'en-tête par défaut
+                                      contentStyle: {
+                                          backgroundColor: 'transparent', // Supprime le fond des écrans
+                                      },
+                                  }}
+                              >
+                                  <Stack.Screen name="Home" component={Home}/>
+                                  <Stack.Screen name="Forecast" component={Forecast}/>
+                              </Stack.Navigator>
+                          )  : null}
+                      </SafeAreaView>
+                  </SafeAreaProvider>
+              </ ImageBackground>
+        </NavigationContainer>
+      </NavigationIndependentTree>
+
   );
 }
