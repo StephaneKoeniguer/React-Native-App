a7095e22c55e2520daf19705dc3c6a1cb1fafe1c
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2025-01-24 13:50:27 +0100

    feature meteo advanced header

--- meteo-app/pages/Forecast/Forecast.jsx
+++ meteo-app/pages/Forecast/Forecast.jsx
@@ -1,17 +1,33 @@
 import { s } from './Forecast.style';
 import {Txt} from "@/components/Txt/Txt";
 import {Container} from "@/components/Container/Container";
-import {useRoute} from "@react-navigation/native";
+import {useNavigation, useRoute} from "@react-navigation/native";
+import {TouchableOpacity, View} from "react-native";
 
 export function Forecast({}) {
 
     const {params} = useRoute();
+    const nav = useNavigation()
 
-    console.log(params);
+    const backButton = (
+        <TouchableOpacity style={s.button} onPress={()=>nav.goBack()}>
+            <Txt> {"<"}</Txt>
+        </TouchableOpacity>
+    );
+    const header = (
+        <View style={s.header}>
+            {backButton}
+            <View style={s.header_texts}>
+                <Txt>{params.city}</Txt>
+                <Txt style={s.subtitle}>Prévision sur 7 jours</Txt>
+            </View>
+        </View>
+    );
+
 
     return (
         <Container>
-            <Txt>Forecast</Txt>
+            {header}
         </Container>
     )
 }
--- meteo-app/pages/Forecast/Forecast.style.js
+++ meteo-app/pages/Forecast/Forecast.style.js
@@ -2,6 +2,19 @@ import { StyleSheet} from "react-native";
 
 
 export const s = StyleSheet.create({
-
+    header: {
+        flexDirection: 'row'
+    },
+    header_texts: {
+        flex: 1,
+        alignItems: 'center',
+        marginRight: 30
+    },
+    subtitle: {
+        fontSize: 20
+    },
+    button: {
+       width: 30
+    }
 
 })
